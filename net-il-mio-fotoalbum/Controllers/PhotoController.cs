a382535cd827839c5df77af94eb5e7dbba6fa05c
Gabriele <gabriele.palma.bool@gmail.com>
2024-05-28 11:09:52 +0200

    add

---
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class PhotoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(PhotoManager.GetAllPhotos(true));
+        }
+    }
+}
